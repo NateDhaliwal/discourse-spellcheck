53a278e6f2dcda5682a3ef56914378009a106cdd
NateDhaliwal <nathaniel.shaan@gmail.com>
2025-09-15 20:05:00 +0800

    Update todo.js

--- javascripts/discourse/api-initializers/todo.js
+++ javascripts/discourse/api-initializers/todo.js
@@ -3,7 +3,6 @@ import { apiInitializer } from "discourse/lib/api";
 export default apiInitializer((api) => {
   api.decorateCookedElement((element) => {
     console.log(element.innerHTML);
-    const dictionary = new Typo("en_US", false, false, { dictionaryPath: "typo/dictionaries" }),
-    
+    const dictionary = new Typo("en_US", false, false, { dictionaryPath: "typo/dictionaries" });
   });
 });
