c1320ddb48e8b5e32e5a212525cc9e8465858757
NateDhaliwal <nathaniel.shaan@gmail.com>
2025-09-15 20:09:42 +0800

    Update todo.js

--- javascripts/discourse/api-initializers/todo.js
+++ javascripts/discourse/api-initializers/todo.js
@@ -1,8 +1,15 @@
 import { apiInitializer } from "discourse/lib/api";
 
+const dictionary = new Typo("en_US", false, false, { dictionaryPath: "typo/dictionaries" });
+
 export default apiInitializer((api) => {
   api.decorateCookedElement((element) => {
-    console.log(element.innerText);
-    const dictionary = new Typo("en_US", false, false, { dictionaryPath: "typo/dictionaries" });
+    let post = element.innerText;
+    post.split(' ').forEach((word) => {
+      let isCorrect = dictionary.check(word);
+      if (!isCorrect) {
+        console.log(word + ": " + dictionary.suggest(word));
+      }
+    });
   });
 });
