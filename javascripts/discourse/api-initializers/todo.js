cc9dbf195f3efe00b80ebd9ce73049ebddf8a5c4
NateDhaliwal <nathaniel.shaan@gmail.com>
2025-09-17 08:48:54 +0800

    Update todo.js

--- javascripts/discourse/api-initializers/todo.js
+++ javascripts/discourse/api-initializers/todo.js
@@ -1,16 +1,38 @@
 import { apiInitializer } from "discourse/lib/api";
 
-const dictionary = new Typo("en_US", false, false, { dictionaryPath: "assets/dictionaries" });
-
 export default apiInitializer((api) => {
   api.decorateCookedElement((element) => {
     let post = element.innerText;
     post.split(' ').forEach((word) => {
-      let isCorrect = dictionary.check("mispelling");
-      console.log(word + ": " + isCorrect);
-      if (!isCorrect) {
-        let suggestions = dictionary.suggest("mispelling");
-        console.log(word + ": " + suggestions);
+      const url = 'https://webspellchecker-webspellcheckernet.p.rapidapi.com/api';
+      const options = {
+      	method: 'POST',
+      	headers: {
+      		'x-rapidapi-host': 'webspellchecker-webspellcheckernet.p.rapidapi.com',
+      		'Content-Type': 'application/json'
+      	},
+      	body: {
+      		cmd: 'check',
+      		lang: 'en_US',
+      		text: word.trim(),
+      		disable_grammar: 'true',
+      		disable_spelling: 'false',
+      		disable_style_guide: 'true',
+      		user_dictionary: '',
+      		user_wordlist: 'word1,word2',
+      		ignore_all_caps: '0',
+      		ignore_words_with_numbers: '0',
+      		ignore_mixed_case: '0',
+      		ignore_domain_names: '0'
+      	}
+      };
+      
+      try {
+      	const response = await fetch(url, options);
+      	const result = await response.text();
+      	console.log(result);
+      } catch (error) {
+      	console.error(error);
       }
     });
   });
