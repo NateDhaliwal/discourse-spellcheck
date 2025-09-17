445a655ceb77c3d78bcb90c031492734babd643a
NateDhaliwal <nathaniel.shaan@gmail.com>
2025-09-17 08:58:13 +0800

    Update todo.js

--- javascripts/discourse/api-initializers/todo.js
+++ javascripts/discourse/api-initializers/todo.js
@@ -1,43 +1,32 @@
 import { apiInitializer } from "discourse/lib/api";
 
-async function checkWord(word) {
-  const url = 'https://webspellchecker-webspellcheckernet.p.rapidapi.com/api';
+async function checkText(text) {
+  const url = 'https://ginger4.p.rapidapi.com/correction?lang=US&generateRecommendations=false&flagInfomralLanguage=true';
   const options = {
-    method: 'POST',
-    headers: {
-      'x-rapidapi-host': 'webspellchecker-webspellcheckernet.p.rapidapi.com',
-      'Content-Type': 'application/json'
-    },
-    body: {
-      cmd: 'check',
-      lang: 'en_US',
-      text: word.trim(),
-      disable_grammar: 'true',
-      disable_spelling: 'false',
-      disable_style_guide: 'true',
-      user_dictionary: '',
-      user_wordlist: 'word1,word2',
-      ignore_all_caps: '0',
-      ignore_words_with_numbers: '0',
-      ignore_mixed_case: '0',
-      ignore_domain_names: '0'
-    }
+  	method: 'POST',
+  	headers: {
+  		'x-rapidapi-host': 'ginger4.p.rapidapi.com',
+  		'Content-Type': 'text/plain',
+  		'Accept-Encoding': 'identity'
+  	},
+  	body: text
   };
   
   try {
-    const response = await fetch(url, options);
-    const result = await response.text();
-    console.log(result);
+  	const response = await fetch(url, options);
+  	const result = await response.text();
+  	console.log(result);
   } catch (error) {
-    console.error(error);
+  	console.error(error);
   }
 }
 
 export default apiInitializer((api) => {
   api.decorateCookedElement(async function (element) {
     let post = element.innerText;
-    post.split("[\\s]").forEach(async function eachWord(word) {
-      await checkWord(word);
-    });
+    await checkWord(post.toString());
+    // post.split("[\\s]").forEach(async function eachWord(word) {
+      
+    // });
   });
 });
